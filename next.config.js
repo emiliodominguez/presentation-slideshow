f251a49196ca2c2e0bca5fe2193f63a9f6f55504
Emilio Dominguez <emilioadominguezb@gmail.com>
2021-10-27 20:25:52 -0300

    Updated Next configuration and downgraded eslint dependency

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 module.exports = {
     generateBuildId: () => "build",
     reactStrictMode: true,
+    swcMinify: true,
     images: {
         domains: ["images.prismic.io", "prismic-io.s3.amazonaws.com"]
     }
